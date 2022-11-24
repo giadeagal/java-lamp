fb554ba1ca9c3449adcf8570819849bb05322fdb
Gianluca <gl.galassini@gmail.com>
2022-11-24 14:35:02 +0100

    importo da vscode

--- lamp/Lampadina.java
+++ lamp/Lampadina.java
@@ -1,7 +1,32 @@
 package lamp;
 
-public class Lampadina {
-	
-	
+public class Lampadina{
+    private String currentStatus;
+    private int maxSwitches;
+    private int numClicks;
+    
+    public Lampadina(){
+        this.currentStatus = "spenta";
+        this.maxSwitches = 3;
+    }
+
+    public String stato() {
+        String s = "La lampadina è " + this.currentStatus + ".";
+        return s;
+    }
+
+    public void click() {
+        if (this.numClicks < this.maxSwitches && this.currentStatus == "spenta") {
+                this.currentStatus = "accesa";
+                numClicks++;
+            } else if (this.numClicks < this.maxSwitches && this.currentStatus == "accesa") {
+                this.currentStatus = "spenta";
+                numClicks++;
+            } else {
+                System.out.println("Non succede niente...");
+                this.currentStatus = "rotta";
+                numClicks++;
+            }
+    }
 
 }
---
+++ lamp/TestLampadina.java
@@ -0,0 +1,9 @@
+package lamp;
+
+public class TestLampadina {
+	   public static void main(String[] args) {
+	      Lampadina l1 = new Lampadina();
+	      System.out.println(l1);
+	        
+	   }
+	}
