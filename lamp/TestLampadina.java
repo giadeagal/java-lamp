6e3ef13c1e2d0fea4d009c6420b0511b73c08cf3
Gianluca <gl.galassini@gmail.com>
2022-11-24 15:21:10 +0100

    esercizio click finito

--- lamp/TestLampadina.java
+++ lamp/TestLampadina.java
@@ -3,7 +3,23 @@ package lamp;
 public class TestLampadina {
 	   public static void main(String[] args) {
 	      Lampadina l1 = new Lampadina();
-	      System.out.println(l1);
-	        
+	      
+	      
+	      System.out.println(l1.stato()); //spenta
+	      
+	      l1.click();
+	      System.out.println(l1.stato()); //accesa
+	      
+	      l1.click();
+	      System.out.println(l1.stato()); //spenta
+	      
+	      l1.click();
+	      System.out.println(l1.stato()); //accesa
+	      
+	      l1.click(); // "non succede niente"
+	      System.out.println(l1.stato()); //rotta
+	      
+	      l1.click(); // "non succede niente"
+	      System.out.println(l1.stato()); //rotta
 	   }
 	}
